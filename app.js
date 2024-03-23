625744702fc5b03d62ac42813f4586a562b63d46
Ubuntu <ubuntu@ip-172-31-39-125.ec2.internal>
2024-03-23 22:01:41 +0000

    Basic api setup

--- app.js
+++ app.js
@@ -17,40 +17,58 @@ connection.connect((err) => {
 });
 
 // Create
-app.post('/users', (req, res) => {
-    const { name, age } = req.body;
-    const sql = 'INSERT INTO students (name, age) VALUES (?, ?)';
-    connection.query(sql, [name, age], (err, result) => {
+app.post('/newStudent', (req, res) => {
+    const { first_name, last_name, email } = req.body;
+    const sql = 'INSERT INTO students (first_name, last_name, email) VALUES (?, ?, ?)';
+    connection.query(sql, [first_name, last_name, email], (err, result) => {
         if (err) throw err;
-        res.send('User created successfully');
+        res.send('Student created successfully');
     });
 });
 
 // Read
-app.get('/users', (req, res) => {
-    const sql = 'SELECT * FROM users';
-    connection.query(sql, (err, results) => {
+app.get('/getStudent/:email', (req, res) => {
+    const sql = 'SELECT * FROM students WHERE email = ?';
+    connection.query(sql, [req.params.email], (err, results) => {
         if (err) throw err;
         res.json(results);
     });
 });
 
-// Update
-app.put('/users/:id', (req, res) => {
-    const { name, age } = req.body;
-    const { id } = req.params;
-    const sql = 'UPDATE users SET name = ?, age = ? WHERE id = ?';
-    connection.query(sql, [name, age, id], (err, result) => {
+
+app.put('/updateStudentPersonalInfo', (req, res) => {
+    const { first_name, last_name, email } = req.body;
+    const sql = 'UPDATE students SET first_name = ?, last_name = ? WHERE email = ?';
+    connection.query(sql, [first_name, last_name, email], (err, result) => {
         if (err) throw err;
-        res.send('User updated successfully');
+        res.send('Student created successfully');
+    });
+});
+
+app.put('/updateStudentEmail', (req, res) => {
+    const { first_name, last_name, email } = req.body;
+    const sql = 'UPDATE students SET email = ? WHERE first_name = ? AND last_name = ?';
+    connection.query(sql, [email, first_name, last_name], (err, result) => {
+        if (err) throw err;
+        res.send('Student created successfully');
+    });
+});
+
+// Update
+app.put('/updateStudent/:email/:attr/:value', (req, res) => {
+    const { email ,attr, value } = req.params;
+    const sql = 'UPDATE students SET ? = ? WHERE email = ?';
+    connection.query(sql, [attr, value, email], (err, result) => {
+        if (err) throw err;
+        res.send('Student updated successfully');
     });
 });
 
 // Delete
-app.delete('/users/:id', (req, res) => {
-    const { id } = req.params;
-    const sql = 'DELETE FROM users WHERE id = ?';
-    connection.query(sql, [id], (err, result) => {
+app.delete('/deleteStudent/:email', (req, res) => {
+    const { email } = req.params;
+    const sql = 'DELETE FROM students WHERE email = ?';
+    connection.query(sql, [email], (err, result) => {
         if (err) throw err;
         res.send('User deleted successfully');
     });
